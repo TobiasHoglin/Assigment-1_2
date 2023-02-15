14643315f478a75ad4de5ff3b71a1ad66db33b23
TobiasHoglin <89868005+TobiasHoglin@users.noreply.github.com>
2023-02-15 15:38:31 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+
+#include "deck.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    Deck deck = Deck();
+    cout << "Fresh deck: " << deck << endl;
+    deck.shuffle();
+    cout << "Shuffled deck: " << deck << endl;
+    deck.sort();
+    cout << "Sorted deck: " << deck << endl;
+}
